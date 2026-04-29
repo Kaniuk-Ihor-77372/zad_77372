9cf7087cfada5811a4b459a8bfeaeb1a65bac1a9
Kaniuk-Ihor-77372 <ikaniuk1@stu.vistula.edu.pl>
2026-04-29 17:21:31 +0200

    Create script.js

---
+++ zadanie6_Dane_z_JSON/script.js
@@ -0,0 +1,52 @@
+let currentTheme = "green";
+
+function changeTheme() {
+    const theme = document.getElementById("theme");
+
+    if (currentTheme === "green") {
+        theme.href = "red.css";
+        currentTheme = "red";
+    } else {
+        theme.href = "green.css";
+        currentTheme = "green";
+    }
+}
+
+function toggleSection() {
+    const section = document.getElementById("projekty");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
+
+function validateForm() {
+
+    let imie = document.getElementById("imie").value;
+    let nazwisko = document.getElementById("nazwisko").value;
+    let email = document.getElementById("email").value;
+    let wiadomosc = document.getElementById("wiadomosc").value;
+    let error = document.getElementById("error");
+
+    error.innerHTML = "";
+
+    if (imie === "" || nazwisko === "" || email === "" || wiadomosc === "") {
+        error.innerHTML = "Wszystkie pola są wymagane!";
+        return false;
+    }
+
+    if (/\d/.test(imie) || /\d/.test(nazwisko)) {
+        error.innerHTML = "Imię i nazwisko nie mogą zawierać cyfr!";
+        return false;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+        error.innerHTML = "Niepoprawny email!";
+        return false;
+    }
+
+    alert("Formularz wysłany poprawnie!");
+    return true;
+}
